3574ad1a1c138d6155e04af57edf84a6f880d183
milind-soorya <milind.soorya@vcinnovations.co.uk>
2023-12-21 13:48:30 +0000

    first commit

--- pages/api/oauth.js
+++ pages/api/oauth.js
@@ -1,38 +1,79 @@
 import { getURLWithQueryParams } from "../../helpers/auth";
 
-
 const Oauth = async (req, res) => {
-  // Query to exchange our authorization code for an access token 
+  try {
+    
+    // Query to exchange our authorization code for an access token 
     const LINKEDIN_URL = getURLWithQueryParams(
-        "https://www.linkedin.com/oauth/v2/accessToken",
-        {
-          grant_type: "authorization_code",
-          code: req.query.code,
-          redirect_uri: process.env.LINKEDIN_REDIRECT,
-          client_id: process.env.LINKEDIN_CLIENT_ID,
-          client_secret: process.env.LINKEDIN_CLIENT_SECRET
-        }
-      );
-      let tok;
-      let resp = await fetch(LINKEDIN_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" }
-      });
-      if (resp.ok) tok = await resp.json();
+      "https://www.linkedin.com/oauth/v2/accessToken",
+      {
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: process.env.LINKEDIN_REDIRECT,
+        client_id: process.env.LINKEDIN_CLIENT_ID,
+        client_secret: process.env.LINKEDIN_CLIENT_SECRET
+      }
+    );
 
-        let { access_token, expires_in } = tok;
+    let resp = await fetch(LINKEDIN_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    });
 
-// Query to exchange our token for user infos
-  let auth = "Bearer " + access_token;
-  let u = {};
-  let usr = await fetch("https://api.linkedin.com/v2/me", {
-    method: "GET",
-    headers: { Connection: "Keep-Alive", Authorization: auth }
-  });
-  if (usr.ok) u = await usr.json();
-if (u.localizedFirstName) {
-  res.redirect(`/hello/${u.localizedFirstName}`)
-}
+    if (!resp.ok) {
+      console.error('LinkedIn API error:', resp.statusText);
+      res.status(500).send('Error exchanging authorization code for access token');
+      return;
+    }
+
+    const tok = await resp.json();
+
+    if (!tok || !tok.access_token) {
+      console.error('Token is not defined or invalid');
+      res.status(500).send('Error retrieving access token from LinkedIn');
+      return;
+    }
+
+    const { access_token, expires_in } = tok;
+
+    // Query to exchange our token for user info
+    const auth = "Bearer " + access_token;
+    let u = {};
+    const usr = await fetch("https://api.linkedin.com/v2/userinfo", {
+      method: "GET",
+      headers: { Connection: "Keep-Alive", Authorization: auth }
+    });
+
+    if (!usr.ok) {
+      console.error('LinkedIn API error:', usr.statusText);
+      res.status(500).send('Error retrieving user information from LinkedIn');
+      return;
+    }
+
+    u = await usr.json();
+
+    if (u.given_name) {
+      // Assuming additionalParams is an object containing additional parameters
+      const additionalParams = {
+        name : u.name,
+        firstName: u.given_name,
+        lastName: u.family_name,
+        email: u.email,
+        picture: u.picture
+        // Add more parameters as needed
+      };
+    
+      const queryString = new URLSearchParams(additionalParams).toString();
+    
+      res.redirect(`/hello/${u.given_name}?${queryString}`);
+    } else {
+      console.error('User info not available');
+      res.status(500).send('Error: User information not available');
+    }
+  } catch (error) {
+    console.error('An unexpected error occurred:', error);
+    res.status(500).send('Internal server error');
+  }
 };
 
-export default Oauth
+export default Oauth;
--- pages/hello/[name].js
+++ pages/hello/[name].js
@@ -3,13 +3,27 @@ import { useRouter } from 'next/router';
 
 const Index = () => {
   const router = useRouter();
-  const { name } = router.query;
+  const { name, email, picture } = router.query;
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}> Hello {name}</h1>
+        <h1 className={styles.title}>Hello {name}</h1>
+
+        {email && (
+          <p>
+            Email: <strong>{email}</strong>
+          </p>
+        )}
+
+        {picture && (
+          <img src={picture} alt="User profile" className={styles.profileImage} />
+        )}
+        
+        {/* Add more components for other parameters as needed */}
       </main>
     </div>
   );
 };
+
 export default Index;
